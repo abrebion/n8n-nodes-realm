7d488047e61651f6ce6ec8509463de4066e82f8d
Anthony Brebion <anthony@gmail.com>
2025-11-02 21:02:42 +0100

    Remove dist folder from repo

--- dist/nodes/Realm/GenericFunctions.d.ts
+++
@@ -1,2 +0,0 @@
-import type { IDataObject, IExecuteFunctions, IHookFunctions, IHttpRequestMethods, ILoadOptionsFunctions } from 'n8n-workflow';
-export declare function realmApiRequest(this: IExecuteFunctions | ILoadOptionsFunctions | IHookFunctions, method: IHttpRequestMethods, path: string, body?: IDataObject, qs?: IDataObject): Promise<IDataObject>;
